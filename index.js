ffe64da37c575e0f5e48df0d376658a516ebd232
Matthew Bauer <mjbauer95@gmail.com>
2015-09-23 17:35:58 -0500

    Add service worker.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import games from 'openvgdb'
 import GamesView from './games-view'
 
+if (navigator.serviceWorker)
+  navigator.serviceWorker.register('worker.js')
+
 var view = new GamesView()
 view.games = games.filter(function(game) {
   return game.systemShortName === 'SNES'
